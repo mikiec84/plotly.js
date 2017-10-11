909efdfd102161c5dae15aa5c9caf3a022ce8878
Etienne Tétreault-Pinard <etienne@plot.ly>
2017-10-11 14:40:37 -0400

    dummy

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -15,8 +15,10 @@ case $1 in
         ;;
 
     image)
-        npm run test-image      || EXIT_STATE=$?
+        npm run test-image -- 10 || EXIT_STATE=$?
+        npm run test-image -- "\!\(gl3d_*|gl2d_*\)"
         npm run test-image-gl2d || EXIT_STATE=$?
+        npm run test-image -- gl3d_* || EXIT_STATE=$?
         npm run test-export     || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
